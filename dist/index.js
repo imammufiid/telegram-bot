af102e892a8bc0121b0d49b87c63970803d18407
imammufiid <imufiid98@gmail.com>
2024-06-22 21:57:19 +0700

    feat: Added express

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,21 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const telegram_1 = require("./bot/telegram");
+const express_1 = __importDefault(require("express"));
+const index = (0, express_1.default)();
+const port = process.env.PORT || 8080;
+index.get('/', (_req, res) => {
+    return res.send('Express Typescript on Vercel');
+});
+index.get('/ping', (_req, res) => {
+    return res.send('pong 🏓');
+});
+index.listen(port, () => {
+    return console.log(`Server is listening on ${port}`);
+});
 (0, telegram_1.telegram)().then(() => {
     console.log("Starting telegram bot");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
 import {telegram} from "./bot/telegram";
+import express, { Request, Response } from 'express'
+
+const index = express()
+const port = process.env.PORT || 8080
+
+index.get('/', (_req: Request, res: Response) => {
+  return res.send('Express Typescript on Vercel')
+})
+
+index.get('/ping', (_req: Request, res: Response) => {
+  return res.send('pong 🏓')
+})
+
+index.listen(port, () => {
+  return console.log(`Server is listening on ${port}`)
+})
 
 telegram().then(() => {
   console.log("Starting telegram bot")
