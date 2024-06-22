52e3e21efb14eb8331694fb15bc9e5fe94f5d646
imammufiid <imufiid98@gmail.com>
2024-06-22 19:31:20 +0700

    feat: Added express

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,23 @@
 import {Telegram} from "./bot/telegram";
+import express, { Request, Response } from 'express'
 
 Telegram()
   .then(() => {
     console.info("Starting telegram bot")
   })
+
+
+const app = express()
+const port = process.env.PORT || 8080
+
+app.get('/', (_req: Request, res: Response) => {
+  return res.send('Express Typescript on Vercel')
+})
+
+app.get('/ping', (_req: Request, res: Response) => {
+  return res.send('pong 🏓')
+})
+
+app.listen(port, () => {
+  return console.log(`Server is listening on ${port}`)
+})
