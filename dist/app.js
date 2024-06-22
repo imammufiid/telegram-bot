90dc9057e2b9b49580d1f035acc23940d06e90df
imammufiid <imufiid98@gmail.com>
2024-06-22 19:35:04 +0700

    start bot

--- dist/app.js
+++ dist/app.js
@@ -3,11 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const telegram_1 = require("./bot/telegram");
 const express_1 = __importDefault(require("express"));
-/*Telegram()
-  .then(() => {
-    console.info("Starting telegram bot")
-  })*/
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
 app.get('/', (_req, res) => {
@@ -18,5 +15,9 @@ app.get('/ping', (_req, res) => {
 });
 app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);
+});
+(0, telegram_1.Telegram)()
+    .then(() => {
+    console.info("Starting telegram bot");
 });
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,6 @@
 import {Telegram} from "./bot/telegram";
 import express, { Request, Response } from 'express'
 
-/*Telegram()
-  .then(() => {
-    console.info("Starting telegram bot")
-  })*/
-
-
 const app = express()
 const port = process.env.PORT || 8080
 
@@ -21,3 +15,8 @@ app.get('/ping', (_req: Request, res: Response) => {
 app.listen(port, () => {
   return console.log(`Server is listening on ${port}`)
 })
+
+Telegram()
+  .then(() => {
+    console.info("Starting telegram bot")
+  })
